8db040887ccb291f6e1978112148ee1b3a23340b
Arthur Warmling <arthurwarmling1@gmail.com>
2023-05-06 14:43:51 -0300

    feat: add cors;

--- src/data/services/ApiService.ts
+++ src/data/services/ApiService.ts
@@ -6,6 +6,10 @@ export const ApiService = axios.create({
     baseURL: url,
     headers: {
         "Content-Type": 'application/json',
-        Accept: 'application/json'
+        Accept: 'application/json',
+        "Access-Control-Allow-Credentials": 'true',
+        "Access-Control-Allow-Origin": '*',
+        "Access-Control-Allow-Methods": 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+        "Access-Control-Allow-Headers": 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
     }
 });
